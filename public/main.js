95bbcd1a4f4abdcdf25a322571a4aef26a3b247c
Phoenix255D <2330469@upv.edu.mx>
2025-12-01 05:50:57 -0600

    Gym leader - Pokemon X

--- public/main.js
+++ public/main.js
@@ -542,7 +542,7 @@ function dibujar() {
             let oStep = 1;
             if (imagenesListas && imagenes.jugador && imagenes.jugador.complete) {
                 ctx.globalAlpha = 0.7;
-                if(otroJugador.x != otroJugador.realX || otroJugador.y != otroJugador.realY){
+
                     getX -= (otroJugador.realX - otroJugador.x) * 0.08;
                     getY -= (otroJugador.realY - otroJugador.y) * 0.08; 
                     console.log("X: "+getX);
@@ -552,6 +552,8 @@ function dibujar() {
                         y: getY
                     });
 
+
+                if(otroJugador.x != otroJugador.realX || otroJugador.y != otroJugador.realY){
                     currentIndex += 0.5;
                     oStep = states[Math.floor(currentIndex) % 4];
                 }
